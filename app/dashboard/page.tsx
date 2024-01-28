b70f301ac5f2db0f81f76bbcad4b0a617875ae02
iVinci-AkitoYOSHIMURA <akito.yoshimura@i-vinci.co.jp>
2024-01-28 17:40:43 +0900

    next-dashboard-stated

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,8 @@
+import AcmeLogo from '@/app/ui/acme-logo';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import Image from 'next/image';
+
+export default function Page() {
+  return <div>dashboard component</div>;
+}
